c82289bef5cd69b7b21c4eb671a91f3fba22b7fa
OscarAraya18 <osaronaragar15@gmail.com>
2022-03-07 13:49:24 -0600

    Tutoría III

---
+++ TutoriaIII_es_AR.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_AR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>Tutoría III</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <source>Imprimir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>Esperando mensaje</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
